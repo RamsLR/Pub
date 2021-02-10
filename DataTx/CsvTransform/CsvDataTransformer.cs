30553918a375440c1b04dbb5ab0b0f245fae71f8
RamsLR <Rams@outlook.com>
2021-02-10 15:34:27 -0800

    more untested updates to version 0.1

--- DataTx/CsvTransform/CsvDataTransformer.cs
+++ DataTx/CsvTransform/CsvDataTransformer.cs
@@ -1,8 +1,96 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace CsvTransform
 {
-	public class Class1
+	public class CsvDataFieldTransforms
 	{
+		public int[] CsvFieldPositions;
+		public IFieldTransform FieldTransformer;
+
+		public CsvDataFieldTransforms(int[] fieldPositions, IFieldTransform fieldTransformer)
+		{
+			if (fieldPositions == null || fieldPositions.Length == 0) { throw new ArgumentException(nameof(fieldPositions));  }
+			CsvFieldPositions = fieldPositions;
+			FieldTransformer = fieldTransformer;
+		}
+	}
+
+	public class TransformLog
+	{
+		public readonly int LineNumber;
+		public readonly string InputLine;
+		public readonly string ErrorMessage;
+
+		public TransformLog(int lineNum, string input, string error)
+		{
+			LineNumber = lineNum;
+			InputLine = input;
+			ErrorMessage = error;
+		}
+	}
+
+	public class TransformerOutput
+	{
+		public List<string> OutputLines;
+		public List<TransformLog> Errors;
+	}
+
+	public class CsvDataTransformer
+	{
+		private const char csvDelim = ',';
+		private List<string> csvData;
+		private CsvDataFieldTransforms[] outputFieldTransforms;
+		public CsvDataTransformer(CsvDataFieldTransforms[] dataFieldTransforms)
+		{
+			if (dataFieldTransforms == null || dataFieldTransforms.Length == 0) { throw new ArgumentException(nameof(dataFieldTransforms)); }
+
+			this.outputFieldTransforms = dataFieldTransforms;
+		}
+
+		public TransformerOutput Transform(List<string> inputLines, int startLineNo)
+		{
+			TransformerOutput result = new TransformerOutput()
+			{
+				OutputLines = new List<string>(),
+				Errors = new List<TransformLog>()
+			};
+
+			Parallel.For(0, inputLines.Count, (index) =>
+			{
+				TransformLog logOutput;
+				var output = TransformInputLine(startLineNo + index, inputLines[index], out logOutput);
+
+				result.OutputLines.Add(output);
+				if (logOutput != null) { result.Errors.Add(logOutput); }
+			});
+
+			return result;
+		}
+
+		private string TransformInputLine(int lineNum, string currentLine, out TransformLog logOutput)
+		{
+			List<string> outputFields = new List<string>();
+			logOutput = null;
+
+			if (string.IsNullOrWhiteSpace(currentLine)) 
+			{
+				logOutput = new TransformLog(lineNum, currentLine, "Found line with no valid input fields.");
+				return string.Empty;
+			}
+
+			string[] fields = currentLine.Split(csvDelim);
+			for(int i = 0; i < outputFieldTransforms.Length; ++i)
+			{
+				var inputFields = outputFieldTransforms[i].CsvFieldPositions.Select(p => fields[i]).ToArray();
+				var transformedField = outputFieldTransforms[i].FieldTransformer.TransformField(inputFields);
+				outputFields.Add(transformedField);
+			}
+
+			return string.Join(csvDelim.ToString(), outputFields);
+		}
 	}
 }
--- DataTx/CsvTransform/FieldTransformer.cs
+++ DataTx/CsvTransform/FieldTransformer.cs
@@ -1,10 +1,135 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿/**
+ * This file contains classes to transform a input string format to another based on an interface pattern
+ * The consumer needs to use the factory class to pick the required implementation class of that interface.
+ * The implementation class will perform both the required input data validation and transformation and 
+ * will give appropriate exceptions/errors when it is unsuccessful.
+ * **/
+
+using System;
+using System.Globalization;
+using System.Linq;
 
 namespace CsvTransform
 {
-	class FieldTransformer
+	public interface IFieldTransform
 	{
+		string TransformField(params string[] inputFields);
+	}
+
+	public class FieldTransformFactory
+	{
+		public IFieldTransform GetFieldTransformer(string name)
+		{
+			switch (name)
+			{
+				case "Number": return new NumberFieldTransformer();
+
+				case "DateTime": return new DateTimeFieldTransformer();
+
+				case "ProductId": return new ProductIdFieldTransformer();
+
+				case "ProductName": return new ProductNameFieldTransformer();
+
+				case "Quantity": return new QuantityFieldTransformer();
+
+				default: throw new NotImplementedException();
+			}
+		}
+	}
+
+	public class NumberFieldTransformer : IFieldTransform
+	{
+		public string TransformField(params string[] inputFields)
+		{
+			if (inputFields == null || inputFields.Length != 1 || string.IsNullOrEmpty(inputFields[0]))
+			{
+				throw new ArgumentException(nameof(inputFields));
+			}
+
+			var result = inputFields[0].Trim();
+			if (string.IsNullOrEmpty(result) || !Char.IsDigit(result[0]))
+			{
+				throw new ArgumentException(nameof(inputFields));
+			}
+
+			return ulong.TryParse(inputFields[0], out _) ? result : throw new ArgumentException(nameof(inputFields));
+		}
+	}
+
+	public class DateTimeFieldTransformer : IFieldTransform
+	{
+		public string TransformField(params string[] inputFields)
+		{
+			if (inputFields == null || inputFields.Length != 3)
+			{
+				throw new ArgumentException(nameof(inputFields));
+			}
+
+			try
+			{
+				var result = new DateTime(int.Parse(inputFields[0]), int.Parse(inputFields[1]), int.Parse(inputFields[2])));
+				return result.ToString("d");
+			}
+			catch (Exception e)
+			{
+				throw new ArgumentException($"Invalid input date field - {e} ", nameof(inputFields));
+			}
+		}
+	}
+
+	public class ProductIdFieldTransformer : IFieldTransform
+	{
+		public string TransformField(params string[] inputFields)
+		{
+			if (inputFields == null || inputFields.Length != 1 || string.IsNullOrEmpty(inputFields[0]))
+			{
+				throw new ArgumentException(nameof(inputFields));
+			}
+
+			var result = inputFields[0].Trim();
+			if (string.IsNullOrEmpty(result) || result.All(c => Char.IsLetterOrDigit(c)))
+			{
+				throw new ArgumentException(nameof(inputFields));
+			}
+
+			return result;
+		}
+	}
+
+	public class ProductNameFieldTransformer : IFieldTransform
+	{
+		public string TransformField(params string[] inputFields)
+		{
+			if (inputFields == null || inputFields.Length != 1 || string.IsNullOrEmpty(inputFields[0]))
+			{
+				throw new ArgumentException(nameof(inputFields));
+			}
+
+			var result = inputFields[0].Trim();
+			result = new CultureInfo("en-US", false).TextInfo.ToTitleCase(result);
+
+			return result;
+		}
+	}
+
+	public class QuantityFieldTransformer : IFieldTransform
+	{
+		public string TransformField(params string[] inputFields)
+		{
+			if (inputFields == null || inputFields.Length != 1 || string.IsNullOrEmpty(inputFields[0]))
+			{
+				throw new ArgumentException(nameof(inputFields));
+			}
+
+			try
+			{
+				var result = decimal.Parse(inputFields[0]);
+				return string.Format("{0:.##}", result);
+			}
+			catch (Exception e)
+			{
+				throw new ArgumentException($"Invalid input quantity field - {e} ", nameof(inputFields));
+			}
+		}
 	}
 }
